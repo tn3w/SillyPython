9e2d35cf2b5112535628b997ab64e4f172122839
TN3W <141021574+tn3w@users.noreply.github.com>
2023-09-08 18:31:15 +0000

    Code fixed

--- sillypython.py
+++ sillypython.py
@@ -2,38 +2,52 @@ import random as rand
 import sys
 import os
 import json
+from base64 import b64encode
 
 def sillyfy(python_code: str):
     unique_characters = list(set(python_code))
     new_character_meanings = dict()
+    character_used = []
     for character in unique_characters:
         random_character = rand.choice(unique_characters)
-        while random_character in new_character_meanings:
+        while random_character in character_used:
             random_character = rand.choice(unique_characters)
+        character_used.append(random_character)
+        if character == random_character:
+            continue
         new_character_meanings[character] = random_character
-    new_python_code = python_code
-    for character, new_character in new_character_meanings.items():
-        new_python_code = new_python_code.replace(character, new_character)
-    return new_python_code, new_character_meanings
+    
+    new_python_code = list(python_code)
+    for character, new_character_meaning  in new_character_meanings.items():
+        for char_index, char in enumerate(python_code):
+            if char == character:
+                new_python_code[char_index] = new_character_meaning
+    return ''.join(new_python_code), new_character_meanings
 
 def random():
     return ''.join(rand.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz") for _ in range(rand.choice([3, 4, 5, 6, 7, 8, 9, 10, 11])))
 
 def create_template(silly_code: str, replaces: str):
-    r1, r2, r3, r4, r5 = random(), random(), random(), random(), random()
-    while len(set([r1, r2, r3, r4, r5])) < 5:
-        r1, r2, r3, r4, r5 = random(), random(), random(), random(), random()
+    r1, ra, rb, r2, r3, r4, r5, r6, r7, r8, r9 = random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random()
+    while len(set([r1, ra, rb, r2, r3, r4, r5])) < 5:
+        r1, ra, rb, r2, r3, r4, r5, r6, r7, r8, r9 = random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random(), random()
 
     replaces = json.dumps(replaces)
-    template = f'''
-    {r1} = {replaces}
+    template = f'''from base64 import b64decode as {r4}
+{r1} = {replaces}
+{r1} = dict(({ra}, {rb}) for {rb}, {ra} in {r1}.items())
+
+def {r2}({r3}):
+    {r3} = {r4}({r3}).decode()
+    {r5} = list({r3})
+    for {r6}, {r7} in {r1}.items():
+        for {r8}, {r9} in enumerate({r3}):
+            if {r9} == {r6}:
+                {r5}[{r8}] = {r7}
+    return ''.join({r5})
     
-    def {r2}({r3}):
-        for {r4}, {r5} in {r1}:
-            {r3} = {r3}.replace({r5}, {r4})
-        return {r3}
-    
-    exec({r2}("""{silly_code}"""))'''
+exec({r2}("""{silly_code}"""))'''
+    return template
 
 if __name__ == "__main__":
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -54,7 +68,7 @@ if __name__ == "__main__":
         print("-a, --about               > Displays an About menu with information about the software")
         print("-h, --help                > Displays this help menu")
         print("-f, --file <filename>     > Define the file which should look silly")
-        print("-i, --iterations <number> > How many layers of sillyity there should be. (Default: 10000)")
+        print("-i, --iterations <number> > How many layers of sillyity there should be. (Default: 20)")
     elif not "-f" in sys.argv and not "--file" in sys.argv:
         print("[Error] No file was specified with -f or --file. For more information use -h or --help.")
     else:
@@ -70,7 +84,7 @@ if __name__ == "__main__":
             if not os.path.isfile(file_path):
                 print(f"[Error] The given file '{file_path}' either does not exist or no file was specified after -f or --file.")
             else:
-                iterations = 10000
+                iterations = 20
                 is_error = False
                 if "-i" in sys.argv or "--iterations" in sys.argv:
                     if "-i" in sys.argv:
@@ -97,19 +111,39 @@ if __name__ == "__main__":
                             code = file.read()
                     except Exception as e:
                         print(f"[Error] File '{file_path}' could not be read, error information: {e}")
-                    for _ in range(iterations):
-                        sily_code, replaces = sillyfy(code)
-                        code = create_template(sily_code, replaces)
-                    new_file_path = os.path.join(os.path.dirname(file_path), "sillypy.py")
-                    if os.path.isfile():
-                        print(f"[Error] File '{new_file_path}' could not be written to because it already exists.")
                     else:
-                        try:
-                            with open(new_file_path, "w") as file:
-                                file.write(new_file_path)
-                        except Exception as e:
-                            print(f"[Error] Could not write to file '{new_file_path}', error information: {e}")
+                        for _ in range(iterations):
+                            os.system('cls' if os.name == 'nt' else 'clear')
+                            print("""
+███████╗██╗██╗     ██╗  ██╗   ██╗    ██████╗ ██╗   ██╗
+██╔════╝██║██║     ██║  ╚██╗ ██╔╝    ██╔══██╗╚██╗ ██╔╝
+███████╗██║██║     ██║   ╚████╔╝     ██████╔╝ ╚████╔╝ 
+╚════██║██║██║     ██║    ╚██╔╝      ██╔═══╝   ╚██╔╝  
+███████║██║███████╗███████╗██║       ██║        ██║   
+╚══════╝╚═╝╚══════╝╚══════╝╚═╝       ╚═╝        ╚═╝   """)
+                            print("Iteration Count:", _+1)
+                            silly_code, replaces = sillyfy(code)
+                            silly_code = b64encode(silly_code.encode()).decode()
+                            code = create_template(silly_code, replaces)
+                        os.system('cls' if os.name == 'nt' else 'clear')
+                        print("""
+███████╗██╗██╗     ██╗  ██╗   ██╗    ██████╗ ██╗   ██╗
+██╔════╝██║██║     ██║  ╚██╗ ██╔╝    ██╔══██╗╚██╗ ██╔╝
+███████╗██║██║     ██║   ╚████╔╝     ██████╔╝ ╚████╔╝ 
+╚════██║██║██║     ██║    ╚██╔╝      ██╔═══╝   ╚██╔╝  
+███████║██║███████╗███████╗██║       ██║        ██║   
+╚══════╝╚═╝╚══════╝╚══════╝╚═╝       ╚═╝        ╚═╝   """)
+                        new_file_path = os.path.join(os.path.dirname(file_path), "sillypy.py")
+                        if os.path.isfile(new_file_path):
+                            print(f"[Error] File '{new_file_path}' could not be written to because it already exists.")
                         else:
-                            print(f"File '{file_path}' successfully made silly, result stored in file '{new_file_path}'.")
+                            try:
+                                with open(new_file_path, "w") as file:
+                                    file.write(code)
+                            except Exception as e:
+                                print(f"[Error] Could not write to file '{new_file_path}', error information: {e}")
+                            else:
+                                print(f"File '{file_path}' successfully made silly, result stored in file '{new_file_path}'.")
     input("Enter: ")
     exit()
+
